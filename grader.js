e7ee6a870be129074950d1be6c414c642c5d939d
Cesar Carrasco <cesar@machinemode.com>
2013-07-12 04:23:54 +0000

    hw3 part 3

--- grader.js
+++ grader.js
@@ -22,9 +22,9 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,16 +36,16 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioFile = function(buffer) {
+    return cheerio.load(buffer);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkFile = function(buffer, checksfile) {
+    $ = cheerioFile(buffer);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -55,20 +55,48 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var grade = function(buffer) {
+    var checkJson = checkFile(buffer, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <html_url>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.file) {
+      fs.readFile(program.file, function(err, data) {
+        if(err) {
+          console.log(err);
+          process.exit(1);
+        }
+        grade(data);
+      });
+    } else if(program.url) {
+      rest.get(program.url).on('complete', function(data, response) {
+        if(data instanceof Error) {
+          console.log(data.message);
+          process.exit(1);
+        }
+        // console.log(response.raw);
+        // console.log(response.rawEncoded);
+        grade(response.raw);
+      }); 
+    } else {
+       console.log("Please provide a file or url to grade.");
+       process.exit(1);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
